36fdff8d2ea02ffbec738b834cb8b0557fb956c8
ian <hello@cundiff.xyz>
2022-10-08 12:35:47 -0400

    git checkout -branch = a branch called ranch

---
+++ js/site.js
@@ -0,0 +1,7 @@
+import Chaffle from './chaffle'
+
+var elements = document.querySelectorAll("[data-chaffle]");
+Array.prototype.forEach.call(elements, function (el) {
+  var chaffle = new Chaffle(el, { ang: "en", speed: 20, delay: 100 });
+  chaffle.init();
+});
